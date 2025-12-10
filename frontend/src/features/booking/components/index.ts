b9c4c13a558206050152f7cc469e9819719d7f24
Fikre-M <fikreddu@gmail.com>
2025-12-10 15:15:13 -0500

    cleanup: Remove duplicate files and nested directories from merge

--- frontend/src/features/booking/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as BookingForm } from './BookingForm'
-export { default as CartItem } from './CartItem'
-export { default as WaitlistModal } from './WaitlistModal'
--- frontend/src/features/booking/pages/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as BookingPage } from './BookingPage'
-export { default as CartPage } from './CartPage'
