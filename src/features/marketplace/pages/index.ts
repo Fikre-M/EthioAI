ffcd506715b437cdc509a9a1544af0077c6bfd99
Fikre-M <fikreddu@gmail.com>
2025-12-12 13:02:55 -0500

    Product Categories System Completed 2

--- src/features/marketplace/pages/index.ts
+++ src/features/marketplace/pages/index.ts
@@ -1,2 +1,4 @@
 export { default as MarketplacePage } from './MarketplacePage'
 export { default as ProductDetailPage } from './ProductDetailPage'
+export { default as CategoriesPage } from './CategoriesPage'
+export { default as CategoryPage } from './CategoryPage'
