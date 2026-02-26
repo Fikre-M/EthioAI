be50f0defbc0aab8d9a3af1d48bc1dc397fb398a
Fikre-M <fikreddu@gmail.com>
2026-02-26 07:44:34 -0500

    Update Jest Configuration

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,27 +1,61 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: [
-    '**/__tests__/**/*.ts',
-    '**/?(*.)+(spec|test).ts'
-  ],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/__tests__/**/*.test.ts"],
   collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-    '!src/types/**',
-    '!src/scripts/**',
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/**/__tests__/**",
+    "!src/types/**",
+    "!src/config/**",
   ],
-  coverageDirectory: 'coverage',
-  coverageReporters: [
-    'text',
-    'lcov',
-    'html'
-  ],
-  setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
-  testTimeout: 30000,
-  maxWorkers: 1, // Run tests sequentially to avoid database conflicts
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+    "src/services/AuthService.ts": {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  coverageDirectory: "coverage",
+  verbose: true,
+  setupFilesAfterEnv: ["<rootDir>/src/test/setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
 };
+
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'node',
+//   roots: ['<rootDir>/src'],
+//   testMatch: [
+//     '**/__tests__/**/*.ts',
+//     '**/?(*.)+(spec|test).ts'
+//   ],
+//   transform: {
+//     '^.+\\.ts$': 'ts-jest',
+//   },
+//   collectCoverageFrom: [
+//     'src/**/*.ts',
+//     '!src/**/*.d.ts',
+//     '!src/types/**',
+//     '!src/scripts/**',
+//   ],
+//   coverageDirectory: 'coverage',
+//   coverageReporters: [
+//     'text',
+//     'lcov',
+//     'html'
+//   ],
+//   setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+//   testTimeout: 30000,
+//   maxWorkers: 1, // Run tests sequentially to avoid database conflicts
+// };
